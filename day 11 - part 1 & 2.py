954f54242f93bb89e5db638aa3a1f945d86133ef
Diderikdm <59435489+Diderikdm@users.noreply.github.com>
2021-11-10 18:41:31 +0100

    Create day 11 - part 1 & 2.py

---
+++ day 11 - part 1 & 2.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+data = 18
+combs = [x for x in list(set(combinations([-1,0,1]*2, 2)))]
+grid = [[int(next(iter(x for x in str((((x + 10) * y) + data) * (x + 10))[-3:-2]), '0')) - 5 for x in range(0,301)] for y in range(0,301)]
+print(len(grid))
+adj = lambda x,y : sum(grid[y+a][x+b] for a,b in combs)
+bests = {(x,y) : adj(x,y) for x in range(1,299) for y in range(1,299)}
+print(','.join([str(k-1) for k in max(bests.items(), key = lambda x: x[1])[0]]))
+bests = {}
+for z in range(3,300):
+    for y in range(300):
+        for x in range(300):
+            if y + z >= 300 or x + z >= 300:
+                continue
+            bests[(x,y,z)] = sum(sum(grid[b][x : x + z]) for b in range(y, y + z))
+print(','.join([str(k) for k in max(bests.items(), key = lambda x: x[1])[0]]))
