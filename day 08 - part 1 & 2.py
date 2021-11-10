40b7aadb5e7ea1d334bbf6dbc65f8dac737d6c99
Diderikdm <59435489+Diderikdm@users.noreply.github.com>
2021-11-10 10:34:20 +0100

    Update day 08 - part 1 & 2.py

--- day 08 - part 1 & 2.py
+++ day 08 - part 1 & 2.py
@@ -1,24 +1,23 @@
-def calc(lst, i = 0):
-    children = lst[i]
-    md = lst[i + 1]
-    i += 2
-    temp = []
-    sm = 0
+def calc(data, index = 0):
+    children, metadata = data[index : index + 2]
+    index += 2
+    child_values = 0
     if children:
+        child_result = []
         for x in range(children):
-            s, i = calc(lst, i)
-            temp += [s]
-        for x in lst[i : i + md]:
-            sm += temp[x - 1 : x][0] if temp[x - 1 : x] else 0
+            child_value, index = calc(data, index)
+            child_result += [child_value]
+        for i in data[index : index + metadata]:
+            child_values += child_result[i - 1 : i][0] if child_result[i - 1 : i] else 0
     else:
-        sm = sum(lst[i : i + md])
-    metadata.append(sum(lst[i : i + md]))
-    i += md
-    return sm, i
+        child_values = sum(data[index : index + metadata])
+    metadatas.append(sum(data[index : index + metadata]))
+    index += metadata
+    return child_values, index
     
 with open("2018day8.txt", 'r') as file:
     data = [int(y) for y in sum([x.split() for x in file.read().splitlines()], [])]
-    metadata = []
-    ans = calc(data[:])[0]
-    print(sum(metadata))
+    metadatas = []
+    ans = calc(data)[0]
+    print(sum(metadatas))
     print(ans)
