8a5e500b46dbe4d62c99112a4719175df9962bb8
Diderikdm <59435489+Diderikdm@users.noreply.github.com>
2021-11-11 22:49:53 +0100

    Create day 13 - part 1 & 2.py

---
+++ day 13 - part 1 & 2.py
@@ -0,0 +1,45 @@
+with open("2018 day13.txt", 'r') as file:
+    data = file.read().splitlines()
+    grid = {}
+    carts = {}
+    cs = '<>^v'
+    nexts = {'^' : lambda x,y : (x,y-1),
+             'v' : lambda x,y : (x,y+1),
+             '<' : lambda x,y : (x-1,y),
+             '>' : lambda x,y : (x+1,y)}
+    dirs = [{'^' : '<', '<' : 'v', 'v' : '>', '>' : '^'},
+            {'^' : '^', '<' : '<', 'v' : 'v', '>' : '>'},
+            {'^' : '>', '<' : '^', 'v' : '<', '>' : 'v'}]
+    corners = {'\\' : {'^' : '<', '<' : '^', '>' : 'v', 'v': '>'},
+                '/' : {'^' : '>', '>': '^', '<' : 'v', 'v' : '<'}}
+    for y, row in enumerate(data):
+        for x, what in enumerate(row):
+            grid[(x,y)] = what if what not in cs else '.'
+            if what in cs:
+                carts[(x,y)] = {'Cart' : what, 'Dir' : 0}
+    crashes = set()
+    p1 = None
+    while len(carts) > 1:
+        next_carts = {}
+        for k,v in sorted(carts.items()):
+            nxt = nexts[v['Cart']](*k)
+            grd = grid[nxt]
+            if k not in next_carts and nxt not in next_carts:
+                next_carts[nxt] = carts[k]
+                if grd in corners:
+                    next_carts[nxt]['Cart'] = corners[grd][carts[k]['Cart']]
+                elif grd == '+':
+                    next_carts[nxt]['Cart'] = dirs[carts[k]['Dir']][carts[k]['Cart']]
+                    next_carts[nxt]['Dir'] = (carts[k]['Dir'] + 1) % 3
+            else:
+                crash = next(iter(x for x in [k, nxt] if x in next_carts))
+                p1 = p1 or ','.join([str(x) for x in crash])
+                crashes.add(crash)
+        else:
+            for x in crashes:
+                next_carts.pop(x)
+            crashes = set()
+            carts = next_carts
+            continue
+    print(p1)
+    print(','.join([str(x) for x in next(iter(carts))]))
